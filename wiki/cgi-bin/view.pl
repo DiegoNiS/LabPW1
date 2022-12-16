2cc4e3b7745498f0907d9e0cdd42359f9549c297
DiegoNina <dninas@unsa.edu.pe>
2022-12-15 23:33:08 -0500

    Funcionamiento parcial del perl que transforma .md en .html

--- wiki/cgi-bin/view.pl
+++ wiki/cgi-bin/view.pl
@@ -17,20 +17,43 @@ print <<HTML;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/estilos.css">
+    <link rel="stylesheet" href="../CSS/estilosV.css">
     <title>Wiki</title>
 </head>
 
 <body>
     <section class="cuerpo">
-        <h1>Wiki - Web</h1>
+        <h1 class="si">Wiki - Web</h1>
 
-        <div class="contenedor">
+        <div class="contenedorV">
 HTML
 
 open(FILE, "../markdownFiles/".$fileName.".md") or die "Error";
 while($line = <FILE>){
-    print $line;
+    if($line =~/^(# )/){
+        print "<h2 class='h1'>".substr($line, 2)."</h2>";
+    }
+    elsif($line =~/^(## )/){
+        print "<h2>".substr($line, 3)."</h2>\n";
+    }
+    elsif($line =~/^(### )/){
+        print "<h3>".substr($line, 4)."</h3>\n";
+    }
+    elsif($line =~/^(#### )/){
+        print "<h4>".substr($line, 5)."</h4>\n";
+    }
+    elsif($line =~/^(##### )/){
+        print "<h5>".substr($line, 6)."</h5>\n";
+    }
+    elsif($line =~/^(###### )/){
+        print "<h6>".substr($line, 7)."</h6>\n";
+    }
+    elsif($line eq "\n"){
+        print "<br>\n";
+    }
+    else{
+        print "<p>".$line."</p>\n";
+    }
 }
 close(FILE);
 
--- wiki/markdownFiles/HolaMundo.md
+++ wiki/markdownFiles/HolaMundo.md
@@ -1,3 +1,10 @@
-# Here you can write your code
-
-## This is an exapmle of an markdown file.
+# Here you can write your code
+## This is an exapmle of an markdown file. 
+### title 3
+##title4
+
+#### title 4     
+este es un 
+texto 
+
+con varias lineas                                
