d78211fa497deb06c9e64aeb3823eaf271cf503a
DanteNy <jarednina72@gmail.com>
2022-12-21 08:11:14 -0500

    Finalización de la primera parte del proyecto

--- wiki/cgi-bin/view.pl
+++ wiki/cgi-bin/view.pl
@@ -4,6 +4,21 @@ use strict;
 use warnings;
 use CGI;
 
+sub comFormato{
+    my $cadena = $_[0];
+    if($cadena =~ /(.*)\*\*\*(.*)\*\*\*(.*)/){
+        return $1."<b><i>".$2."</i></b>".comFormato($3);
+    }elsif($cadena =~ /(.*)\*\*(.*)\*\*(.*)/){
+        return $1."<b>".$2."</b>".comFormato($3);
+    }elsif($cadena =~ /(.*)\*(.*)\*(.*)/){
+        return $1."<i>".$2."</i>".comFormato($3);
+    }elsif($cadena =~ /(.*)_(.*)_(.*)/){
+        return $1."<i>".$2."</i>".comFormato($3);
+    }else{
+        return $cadena;
+    }
+}
+
 my $q = CGI->new;
 my $line;
 my $fileName = $q->param('fileName');
@@ -28,33 +43,40 @@ print <<HTML;
         <div class="contenedorV">
 HTML
 
+my $parraf;
+
 open(FILE, "../markdownFiles/".$fileName.".md") or die "Error";
 while($line = <FILE>){
-    if($line =~/^(# )/){
-        print "<h2 class='h1'>".substr($line, 2)."</h2>";
+    if($line =~/^#* /){ #title section
+        if($line =~/^# /){
+            print "<h2 class='h1'>".substr($line, 2)."</h2>";
+        }
+        elsif($line =~/^## /){
+            print "<h2>".substr($line, 3)."</h2>\n";
+        }
+        elsif($line =~/^### /){
+            print "<h3>".substr($line, 4)."</h3>\n";
+        }
+        elsif($line =~/^#### /){
+            print "<h4>".substr($line, 5)."</h4>\n";
+        }
+        elsif($line =~/^##### /){
+            print "<h5>".substr($line, 6)."</h5>\n";
+        }
+        else {
+            print "<h6>".substr($line, 7)."</h6>\n";
+        }
     }
-    elsif($line =~/^(## )/){
-        print "<h2>".substr($line, 3)."</h2>\n";
+    elsif((length($line)eq 2) && !(length($parraf) eq 0)){
+        print OUT "<p>\n$parraf</p>\n";
+        $parraf="";
     }
-    elsif($line =~/^(### )/){
-        print "<h3>".substr($line, 4)."</h3>\n";
-    }
-    elsif($line =~/^(#### )/){
-        print "<h4>".substr($line, 5)."</h4>\n";
-    }
-    elsif($line =~/^(##### )/){
-        print "<h5>".substr($line, 6)."</h5>\n";
-    }
-    elsif($line =~/^(###### )/){
-        print "<h6>".substr($line, 7)."</h6>\n";
-    }
-    elsif($line eq "\n"){
-        print "<br>\n";
-    }
-    else{
-        print "<p>".$line."</p>\n";
+    elsif(length($line) > 2){
+        $line = comFormato($line);
+        $parraf=$parraf.$line;
     }
 }
+print OUT "<p>\n$parraf</p>\n";
 close(FILE);
 
 print <<HTML;
--- wiki/markdownFiles/HolaMundo.md
+++ wiki/markdownFiles/HolaMundo.md
@@ -1,10 +1,15 @@
-# Here you can write your code
-## This is an exapmle of an markdown file. 
-### title 3
-##title4
-
-#### title 4     
-este es un 
-texto 
-
-con varias lineas                                
+# title 1
+
+## This is an exapmle of an markdown file. 
+
+### title 3
+
+title4
+dsfsdfsf
+sdfsfsdf
+
+e **ste es un* _hola_
+
+texto asjdhlsdfa liags flasf lasgdflag
+
+con varias lineas
